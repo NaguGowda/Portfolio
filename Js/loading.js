faa33e78b31ae2b26c17b9a94a50fbb1065f9372
nagu gowda <nagarajsagar13@gmail.com>
2025-07-28 23:55:10 +0530

    expe image set

---
+++ Js/loading.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader-wrapper");
+  const content = document.getElementById("main-content");
+
+  // Hide loader and show content
+  loader.style.display = "none";
+  content.style.display = "block";
+});
